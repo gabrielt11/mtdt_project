c956d1175c940fc9782a1b305397edd471404684
Gabriel <tataryan98@yandex.ru>
2023-11-11 00:37:41 +0300

    mtdt_project

--- main.py
+++ main.py
@@ -2,10 +2,10 @@ from fastapi import FastAPI
 
 app = FastAPI()
 
-@app.get("/celcius_degrees/{celcius}")
-async def converter(celcius:float):
-    farenheit = 9 / 5 * celcius + 32
-    return {'градусы по шкале Фаренгейта':farenheit}
+@app.get("/celsius_degrees/{celsius}")
+async def converter(celsius:float):
+    fahrenheit = 9 / 5 * celsius + 32
+    return {'градусы по шкале Фаренгейта':fahrenheit}
 
 if __name__ == "__main__":
     import uvicorn
